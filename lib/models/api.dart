63c368e79b1b5984951783e0334542e7dc2044c5
SatriaFerdian <151886715+SatriaFerdian@users.noreply.github.com>
2025-02-26 14:25:31 +0700

    biodata

---
+++ lib/models/api.dart
@@ -0,0 +1,8 @@
+class BaseURL{
+  static String url = "192.168.137.164";
+  static String tambah = "http://$url/biodata/create.php";
+  static String details = "http://$url/biodata/detail.php";
+  static String update = "http://$url/biodata/update.php";
+  static String delete = "http://$url/biodata/delete.php";
+  static String list = "http://$url/biodata/list.php";
+}
